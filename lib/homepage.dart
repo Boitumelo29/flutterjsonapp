e5f3c738178764eef5e41d35faec7c04a34fce52
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-09 11:08:23 +0200

    update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,23 +11,42 @@ class HomePage extends StatelessWidget {
 // we use the future because we are fetching from the api and it can be delayed
   Future getTeams() async {
     var response =
-        await http.get(Uri.https('rickandmortyapi.com', 'api/character/2'));
+        await http.get(Uri.https('rickandmortyapi.com', 'api/character'));
     print("my call ${response.body}");
 
     var jsonData = jsonDecode(response.body);
 
     for (var char in jsonData['results']) {
-      final chart = Character(name: char['name'], species: char['species']);
+      final chart = Character(
+          name: char['name'], species: char['species'], image: char['image']);
+      characters.add(chart);
     }
+
+    print("my other calll===========${characters.length}");
   }
 
   @override
   Widget build(BuildContext context) {
     getTeams();
     return Scaffold(
-      body: Column(
-        children: [],
-      ),
+      body: FutureBuilder(
+          future: getTeams(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ListView.builder(
+                itemCount: characters.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Image.network(characters[index].image),
+                    title: Text(characters[index].name),
+                    subtitle: Text(characters[index].species),
+                  );
+                },
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          }),
     );
   }
 }
--- lib/models/model.dart
+++ lib/models/model.dart
@@ -1,6 +1,7 @@
 class Character {
   final String name;
   final String species;
+  final String image;
 
-  Character({required this.name, required this.species});
+  Character({required this.name, required this.species, required this.image});
 }
