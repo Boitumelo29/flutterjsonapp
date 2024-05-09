9a7ad5792ff028557f959dd0e84c8023414ae610
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-09 09:19:19 +0200

    update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+// we use the future because we are fetching from the api and it can be delayed
+  Future getTeams() async {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
